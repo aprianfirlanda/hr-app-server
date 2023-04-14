55bcee0f63d5381ca75a60280d0acdd69a9fed70
aprianfirlanda <aprianfirlanda@gmail.com>
2023-04-14 14:57:18 +0700

    init project

---
+++ src/main/java/com/aprianfirlanda/hrappserver/HrAppServerApplication.java
@@ -0,0 +1,13 @@
+package com.aprianfirlanda.hrappserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrAppServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrAppServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aprianfirlanda/hrappserver/HrAppServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aprianfirlanda.hrappserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrAppServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
